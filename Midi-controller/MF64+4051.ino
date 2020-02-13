60140ea3b920a61d5f973ce79d434a7049f5d2e6
4dvn <vuha.hau@hyper.vn>
2020-02-13 23:57:54 +0700

    Update MF64+4051.ino

--- Midi-controller/MF64+4051.ino
+++ Midi-controller/MF64+4051.ino
@@ -2,7 +2,7 @@
 
 USBMIDI_Interface midi;
 
-const AddressMatrix<4, 3> addresses = {{
+const AddressMatrix<8, 8> addresses = {{
   {71, 70, 69, 68, 87, 86, 85, 84}, 
   {75, 74, 73, 72, 91, 90, 89, 88},
   {79, 78, 77, 76, 95, 94, 93, 92},
@@ -13,3 +13,9 @@ const AddressMatrix<4, 3> addresses = {{
   {66, 66, 65, 64, 51, 50, 49, 48}
 };
 
+NoteButtonMatrix<8, 8> buttonmatrix = {
+  {0, 1, 2, 3, 4, 5, 6, 7}, // row pins
+  {8, 9, 10, 11, 12, 13, 14},    // column pins
+  addresses,    // address matrix
+  CHANNEL_1,    // channel and cable number
+};
