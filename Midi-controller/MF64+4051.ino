fc4f8a2b869ed646e2c4c367e5759e343f484884
4dvn <vuha.hau@hyper.vn>
2020-02-14 00:21:22 +0700

    Update MF64+4051.ino

--- Midi-controller/MF64+4051.ino
+++ Midi-controller/MF64+4051.ino
@@ -63,30 +63,10 @@ NoteButtonMatrix<8, 8> buttonmatrix = {
   addresses,    // address matrix
   CHANNEL_1,    // channel and cable number
 };
+void setup() {
+  Control_Surface.begin(); // Initialize Control Surface
+}
 
-
-// Transposer to go +1 to -1 octave
-Transposer<-2, +2> transposer(16);
-IncrementDecrementSelector<transposer.getNumberOfBanks()> transposeSelector = {
-  transposer,
-  {15, 16}, // Press push button A0 → +1 octave, A1 → -1 octave, A0+A1 → reset
-  Wrap::Clamp,
-};
-
-const AddressMatrix<8, 8> addresses = {{
-  {55, 54, 53, 52, 71, 70, 69, 68}, 
-  {59, 58, 57, 56, 75, 74, 73, 72},
-  {63, 62, 61, 60, 79, 78, 77, 76},
-  {67, 66, 65, 64, 83, 82, 81, 80},
-  {39, 38, 37, 36, 23, 22, 21, 20},
-  {43, 42, 41, 40, 27, 26, 25, 24},
-  {47, 46, 45, 44, 31, 30, 29, 28},
-  {51, 50, 49, 48, 35, 34, 33, 32}
-};
-
-NoteButtonMatrix<8, 8> buttonmatrix = {
-  {0, 1, 2, 3, 4, 5, 6, 7}, // row pins
-  {8, 9, 10, 11, 12, 13, 14},    // column pins
-  addresses,    // address matrix
-  CHANNEL_1,    // channel and cable number
-};
+void loop() {
+  Control_Surface.loop(); // Update the Control Surface
+}
